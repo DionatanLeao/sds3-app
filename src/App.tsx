e62e25df289c36f9686b49e84eba58bbd49e2456
DionatanLeao <dionatan.ti@outlook.com>
2021-05-03 19:13:48 -0300

    Basic static components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar/index";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className= "text-primary">Semana Spring React</h1>
+      
+    <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
